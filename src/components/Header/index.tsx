4f85339065cc82c5188e01df6b7a3074c2d88510
GuiAquinoDev <guiiaquino92@gmail.com>
2023-04-26 12:00:22 -0300

    feat: finalizando componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
-import { Container } from './style'
+import { Container, Logo, User } from './style'
+
+import logo from '@assets/logo.png'
+import user from '@assets/user.png'
 
 export function Header() {
     return (
         <Container>
-            
+            <Logo source={logo}/>
+            <User source={user}/>
         </Container>
     )
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -2,4 +2,16 @@ import {SafeAreaView} from 'react-native-safe-area-context'
 import styled from "styled-components/native";
 
 export const Container = styled(SafeAreaView)`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+export const Logo = styled.Image`
+    width: 82px;
+    height: 37px;
+`
+export const User = styled.Image`
+    width: 40px;
+    height: 40px;
 `
